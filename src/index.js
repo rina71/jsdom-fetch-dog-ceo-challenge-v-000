e45f2e7717b6d0b7099caa811270cbc66b3b66e7
Yurina Shin <yurinas@gmail.com>
2020-08-04 02:19:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 console.log('%c HI', 'color: firebrick')
+function fetchDogs() {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => renderDogs(json));
+}
+
+function fetchBreeds(){
+  return fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp => resp.json())
+  .then(json => renderBreeds(json));
+}
+
+function renderBreeds(json) {
+  const dropdown = document.getElementById('breed-dropdown');
+  const breeds  = Object.keys(json.message);
+  let newBreeds = dropdown.setAttribute("onchange", function(){
+    breeds.filter(item => {
+      return item.slice(0,1) === dropdown.value;
+  });
+});
+    listItems(newBreeds);
+}
+
+
+
+function listItems(breeds){
+  breeds.forEach(breed => {
+    const ul = document.getElementById('dog-breeds');
+    const li = document.createElement('li');
+    const st = document.createElement('style');
+    li.appendChild(st);
+    li.innerHTML = `${breed}`;
+    li.onclick = function(){
+      li.style.color = '#f15025'
+    };
+    ul.appendChild(li);
+  })
+}
+
+
+function renderDogs(json) {
+  const img = document.getElementById('dog-image-container')
+  json.message.forEach(dog => {
+    const dogImg = document.createElement('img');
+    dogImg.src = dog;
+    dogImg.style.width = "500px";
+    dogImg.style.height = "500px";
+    img.appendChild(dogImg);
+  })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchDogs();
+  fetchBreeds();
+})
